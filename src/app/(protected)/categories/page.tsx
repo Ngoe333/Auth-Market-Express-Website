a76f02aa101f19e8afc31220d1b48736eb1ebf0e
Ngoe333 <essoumangoelovett@gmail.com>
2024-02-08 19:02:05 +0100

    ppp

--- src/app/(protected)/categories/page.tsx
+++ src/app/(protected)/categories/page.tsx
@@ -1,15 +1,30 @@
+import { UserRole } from '@prisma/client'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 import React from 'react'
 
 function CategoriesPage() {
-  return (
-    <section className=' max-w[500px] mt-24 flex mx-auto justify-center'>
+    const path = usePathname();
+    return (
+        <section className=' max-w[500px] mt-24 flex mx-auto justify-center'>
 
-        <div>
+            <div className=' flex flex-wrap gap-2 tags mx-auto justify-center mt-24'>
+                <Link href={'/settings'} className={path === '/settings' ? 'active bg-green-500 text-white rounded-full py-2 px-4 ' : 'bg-gray-300 text-gray-700 rounded-full py-2 px-4 '}>Settings</Link>
+                {UserRole.ADMIN && (
+                    <>
+                        <Link href={'/categories'} className={path === '/categories' ? 'active bg-green-500 text-white rounded-full py-2 px-4 ' : 'bg-gray-300 text-gray-700 rounded-full py-2 px-4 '}>Categories</Link>
+                        <Link href={'/menu-items'} className={path === '/menu-items' ? 'active bg-green-500 text-white rounded-full py-2 px-4 ' : 'bg-gray-300 text-gray-700 rounded-full py-2 px-4 '}>Meu Items</Link>
+                        <Link href={'/users'} className={path === '/users' ? 'active bg-green-500 text-white rounded-full py-2 px-4 ' : 'bg-gray-300 text-gray-700 rounded-full py-2 px-4 '}>Users</Link>
+                    </>
+                )}
+            </div>
 
-        </div>
+            <div>
 
-    </section>
-  )
+            </div>
+
+        </section>
+    )
 }
 
 export default CategoriesPage
