f0542e75e5623f070053f34b6f2ca2a344a16bf9
Ngoe333 <essoumangoelovett@gmail.com>
2024-02-09 15:06:44 +0100

    uuu

--- src/app/(protected)/categories/page.tsx
+++ src/app/(protected)/categories/page.tsx
@@ -9,7 +9,7 @@ function CategoriesPage() {
         <section className=' max-w[500px] mt-24 flex mx-auto justify-center'>
 
             <div className=' flex flex-wrap  space-y-2 gap-2 tags mx-auto justify-center'>
-                <Link href={'/settings'} className={path === '/settings' ? 'active bg-green-500 text-white rounded-full py-2 px-4 ' : 'bg-gray-300 text-gray-700 rounded-full py-2 px-4 '}>Settings</Link>
+                <Link href={'/settings'} className={path === '/settings' ? ' bg-green-500 text-white rounded-full py-2 px-4 ' : 'bg-gray-300 text-gray-700 rounded-full py-2 px-4 '}>Settings</Link>
                 {UserRole.ADMIN && (
                     <>
                         <Link href={'/categories'} className={path === '/categories' ? 'bg-green-500 text-white rounded-full py-2 px-4 ' : 'bg-gray-300 text-gray-700 rounded-full py-2 px-4 '}>Categories</Link>
